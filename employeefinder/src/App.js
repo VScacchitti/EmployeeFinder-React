ca14f3be63c9dc050605818f8cd745629a191a38
Vince Scacchitti <VScacchitti24@gmail.com>
2020-12-15 12:45:05 -0500

    built table and poplates with data from API.

--- employeefinder/src/App.js
+++ employeefinder/src/App.js
@@ -2,12 +2,14 @@
 import './App.css';
 import Wrapper from "./components/Wrapper";
 import Header from "./components/Header"
+import TableArea from "./components/TableArea"
 
 function App() {
   return (
     <div className="App">
       <Wrapper>
         <Header />
+        <TableArea/>
       </Wrapper>
     </div>
   );
--- employeefinder/src/components/Table.js
+++ employeefinder/src/components/Table.js
@@ -2,15 +2,29 @@ import React from "react";
 import TableBody from "../components/TableBody";
 import "../index.css";
 
-function Table({ }) {
+function Table({headings, users, handleSort }) {
     return (
         <div className="datatable mt-5">
             <table className="table table-striped table-hover table-condensed" id="table">
-                <thead>
-                    <tr>
-                        <th></th>
-                    </tr>
-                </thead>
+            <thead>
+          <tr>
+            {headings.map(({ name, width }) => {
+              return (
+                <th
+                  className="col"
+                  key={name}
+                  style={{ width }}
+                  onClick={() => {
+                    handleSort(name.toLowerCase());
+                  }}
+                >
+                  {name}
+                  <span className="pointer"></span>
+                </th>
+              );
+            })}
+          </tr>
+        </thead>
                 < TableBody/>
             </table>
         </div>
