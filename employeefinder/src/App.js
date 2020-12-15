84f51e2a5053bae56086919573f768ec38fbbcaa
Vince Scacchitti <VScacchitti24@gmail.com>
2020-12-15 07:12:15 -0500

    added NavBar and Search bar components.

--- employeefinder/src/App.js
+++ employeefinder/src/App.js
@@ -1,6 +1,7 @@
 
 import './App.css';
-import Wrapper from "./components/Wrapper"
+import Wrapper from "./components/Wrapper";
+import Header from "./components/Header"
 
 function App() {
   return (
